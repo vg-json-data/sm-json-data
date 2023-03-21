9b0fabe5e6ae4c35918421ec8ccfb404514d69ed
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2023-03-20 22:07:08 -0700

    Moar Clean Maps
    Fix Covers
    Add Tourian

--- resources/ci/common/roomDiagrams.py
+++ resources/ci/common/roomDiagrams.py
@@ -3,12 +3,15 @@ Manage Room Diagrams
 '''
 import json
 import os
+import re
 from PIL import Image
 
 def coord_calc(origin,dims):
     x1, x2 = origin
     w, h = dims
     return (x1,x2,w+x1,h+x2)
+
+roomIDs = {}
 
 for r,d,f in os.walk(
     os.path.join(
@@ -17,7 +20,32 @@ for r,d,f in os.walk(
     )
 ):
     for filename in f:
+        if ".json" in filename and "roomDiagrams" not in filename:
+            # JSON Files
+            with open(os.path.join(r, filename), "r", encoding="utf-8") as regionFile:
+                regionJSON = json.load(regionFile)
+                area = ""
+                subarea = ""
+                if "rooms" in regionJSON:
+                    for room in regionJSON["rooms"]:
+                        areaPath = os.path.dirname(os.path.join(r, filename)).split(os.sep)
+                        area = room["area"]
+                        subarea = room["subarea"]
+                        areaSlug = areaPath[-1]
+                        subareaSlug = os.path.splitext(filename)[0]
+                        roomIDs[str(room["id"])] = {
+                            "name": room["name"],
+                            "area": area,
+                            "subarea": subarea,
+                            "areaSlug": areaSlug,
+                            "subareaSlug": subareaSlug
+                        }
+                    print(f"> Reading {area}/{subarea}")
+                else:
+                    print(f"!!! {os.path.join(r,filename)} has no rooms!")
         if ".png" in filename and "region_" in filename:
+            # Region Image
+            print(f" > Opening {filename}")
             if not os.path.isdir(
                 os.path.join(
                     ".",
@@ -49,6 +77,15 @@ for r,d,f in os.walk(
                             if roomID == "origin":
                                 mapOrigin = roomData
                                 continue
+                            subareaSlug = "unknownSubArea"
+                            roomName = "unknownRoom"
+                            if roomID in roomIDs:
+                                areaSlug = roomIDs[roomID]["areaSlug"]
+                                subareaSlug = roomIDs[roomID]["subareaSlug"]
+                                roomName = roomIDs[roomID]["name"]
+                                if areaSlug == "ceres":
+                                    subareaSlug = "ceres"
+                            print(f"  > Building {roomID}: {roomName}")
                             roomOrigin = roomData["origin"]
                             roomOrigin = ((roomOrigin[0] * 256) + mapOrigin[0], (roomOrigin[1] * 256) + mapOrigin[1])
                             width = "width" in roomData and roomData["width"] or 1
@@ -62,45 +99,31 @@ for r,d,f in os.walk(
                                     )
                                 )
                             )
-                            # Wrecked Ship
-                            if "wreckedship" in r:
-                                # Main Shaft
-                                if roomID == "155":
-                                    # Cover Bowling Alley
-                                    black_img = Image.new("RGB",
-                                        (4 * 256, 5 * 256)
-                                    )
-                                    cropped_image.paste(black_img)
-                                    # Cover Super Missile
-                                    black_img = Image.new("RGB",
-                                        (4 * 256, 2 * 256)
+                            if "covers" in roomData:
+                                for cover in roomData["covers"]:
+                                    if "name" in cover:
+                                        print(f"   > Cover: {cover['name']}")
+                                    origin = "origin" in cover and cover["origin"] or [0, 0]
+                                    width = "width" in cover and cover["width"] or 1
+                                    height = "height" in cover and cover["height"] or 1
+                                    black_img = Image.new(
+                                        "RGB",
+                                        (width * 256, height * 256)
                                     )
                                     cropped_image.paste(
                                         black_img,
-                                        (0, 6 * 256)
+                                        (
+                                            origin[0] * 256,
+                                            origin[1] * 256
+                                        )
                                     )
-                                # Bowling Alley
-                                if roomID == "161":
-                                    # Cover Area Map
-                                    black_img = Image.new("RGB",
-                                        (1 * 256, 1 * 256)
-                                    )
-                                    cropped_image.paste(black_img)
-                                    # Cover Gravity Suit
-                                    black_img = Image.new("RGB",
-                                        (1 * 256, 1 * 256)
-                                    )
-                                    cropped_image.paste(
-                                        black_img,
-                                        (0, 2 * 256)
-                                    )
-                            regionName = r.split(os.sep)[2]
+                            roomName = re.sub(r"[^\w\d]", "", roomName)
                             cropped_image.save(
                                 os.path.join(
                                     ".",
                                     r,
                                     "roomDiagrams",
                                     "clean",
-                                    f"{regionName}_{roomID}_roomName.png"
+                                    f"{subareaSlug}_{roomID}_{roomName}.png"
                                 )
                             )
