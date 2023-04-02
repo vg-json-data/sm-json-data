738000222f5b7fffa13926c6024d71ea55f21177
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2023-04-01 22:29:58 -0700

    Make Lookups smarter
    Rename Kraid's stuff to belong to him
    Rename Rooms 68 & 4 to match room defns
    Add subsubareas to Maridia (Fuckin' Maridia)
    Push output from lookups script

--- resources/ci/common/list_lookups.py
+++ resources/ci/common/list_lookups.py
@@ -4,11 +4,12 @@ Export manifests for lookup tables
 '''
 import json
 import os
+import re
 
 data = {
     "regions": {},
     "roomIDsByLCRoomName": {},
-    "roomsByRoomID": {}
+    "roomsByRegion": {}
 }
 regionPath = os.path.join(
     ".",
@@ -21,6 +22,7 @@ for region in os.listdir(regionPath):
     if os.path.isdir(os.path.join(regionPath, region)):
         # add the region to our notes
         data["regions"][region] = []
+        data["roomsByRegion"][region] = {}
         # cycle through files
         for subregion in os.listdir(os.path.join(regionPath, region)):
             # if it's a JSON file and not a roomDiagram
@@ -28,12 +30,27 @@ for region in os.listdir(regionPath):
                 # add the subregion to our notes
                 data["regions"][region].append(os.path.splitext(subregion)[0])
                 # open a subregion file
-                with open(os.path.join(regionPath, region, subregion)) as subregionFile:
+                with open(os.path.join(regionPath, region, subregion), "r", encoding="utf-8") as subregionFile:
                     subregionJSON = json.load(subregionFile)
                     # cycle through rooms
                     for room in subregionJSON["rooms"]:
                         # add LC'd name to our notes
                         data["roomIDsByLCRoomName"][room["name"].lower()] = room["id"]
+                        for stripped in [
+                            room["name"].lower(),
+                            "".join([s for s in room["name"].lower() if s.isalnum() or s.isspace()])
+                        ]:
+                            for [search, repls] in {"": "", "\\W": " "}.items():
+                                for repl in repls:
+                                    stripped = re.sub(r"[" + search + "]+", repl, room["name"].lower())
+                                stripped = re.sub(r"[\s]{2,}", " ", stripped)
+                                stripped = re.sub(r" s ", "s ", stripped)
+                                if stripped != room["name"].lower():
+                                    data["roomIDsByLCRoomName"][stripped] = room["id"]
+
+                        if "ceres" in room["area"].lower():
+                            room["area"] = "Ceres"
+                            room["subarea"] = "Ceres"
 
                         # trim nodes
                         for [i, node] in enumerate(room["nodes"]):
@@ -57,18 +74,49 @@ for region in os.listdir(regionPath):
                                 room["links"][i] = link
 
                         # add roomDiagram path
+                        sanitizedRoomName = room["name"]
+                        for [repl, chars] in {
+                            "": [
+                                "-",
+                                ".",
+                                "(",
+                                ")",
+                                "'"
+                            ],
+                            "-": [
+                                "\\",
+                                "/"
+                            ]
+                        }.items():
+                            for char in chars:
+                                sanitizedRoomName = sanitizedRoomName.replace(char, repl)
+                        sanitizedRoomName = sanitizedRoomName \
+                            .replace(" and ", " And ") \
+                            .replace(" ", "")
+
+                        subarea = room["subarea"].lower()
+
+                        if "subsubarea" in room:
+                            subsubarea = room["subsubarea"]
+                            subarea = f"{subarea.lower()}-{subsubarea.lower()}"
+
                         roomDiagram = os.path.join(
                             # ".",
                             "region",
                             region,
                             "roomDiagrams",
-                            f"{room['subarea'].lower()}_{room['id']}_{room['name'].replace(' and ', ' And ').replace(' ', '')}.png"
+                            f"{subarea}_{room['id']}_{sanitizedRoomName}.png"
                         )
+
                         if os.path.isfile(roomDiagram):
                             room["roomDiagram"] = roomDiagram
+                        else:
+                            print(f"ERROR: {region}:{subarea}:{room['id']}:{room['name']} roomDiagram not found!")
 
                         # add trimmed room to our notes
-                        data["roomsByRoomID"][room["id"]] = room
+                        if subarea.lower() not in data["roomsByRegion"][region]:
+                            data["roomsByRegion"][region][subarea] = {}
+                        data["roomsByRegion"][region][subarea][room["id"]] = room
 
 with open(
     os.path.join(
@@ -76,9 +124,46 @@ with open(
         "resources",
         "app",
         "manifests",
-        "lookups.json"
+        "regions.json"
     ),
     "w",
     encoding="utf-8"
-) as lookupsFile:
-    lookupsFile.write(json.dumps(data, indent=2))
+) as regionsLookupFile:
+    regionsLookupFile.write(json.dumps(data["regions"], indent=2))
+
+with open(
+    os.path.join(
+        ".",
+        "resources",
+        "app",
+        "manifests",
+        "roomIDsByLCRoomName.json"
+    ),
+    "w",
+    encoding="utf-8"
+) as lcNamesFile:
+    lcNamesFile.write(json.dumps(data["roomIDsByLCRoomName"], indent=2))
+
+for region, regionData in data["roomsByRegion"].items():
+    for subregion, subregionData in regionData.items():
+        regionPath = os.path.join(
+            ".",
+            "resources",
+            "app",
+            "manifests",
+            "abridgedRooms",
+            region
+        )
+
+        if not os.path.isdir(regionPath):
+            os.makedirs(regionPath)
+
+        with open(
+            os.path.join(
+                regionPath,
+                f"{subregion}.json"
+            ),
+            "w",
+            encoding="utf-8"
+        ) as subregionFile:
+            subregionFile.write(json.dumps(data["roomsByRegion"][region][subregion], indent=2))
