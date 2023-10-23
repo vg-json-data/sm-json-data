613fdaddaafc11ca08c581bea33b39cf68bb9f81
Brent Kerby <blkerby@gmail.com>
2023-10-22 23:35:24 -0600

    proof of concept of region file restructure & reformat

---
+++ scripts/format_json.py
@@ -0,0 +1,70 @@
+import json
+
+def is_one_liner_dict(obj):
+    if len(obj) == 0:
+        return True
+    if len(obj) == 1:
+        value = next(iter(obj.values()))
+        return isinstance(value, (str, int, float, bool))
+    return False
+
+def is_one_liner_list(obj):
+    if all(isinstance(x, (str, int, float, bool)) for x in obj):
+        return True
+    return False
+
+def format(obj, indent, current_indent=0, one_liner_dict_allowed=True):
+    # print("Formatting {}".format(obj))
+    if isinstance(obj, (str, int, float, bool)) or obj is None:
+        return json.dumps(obj)
+    if isinstance(obj, list):
+        if is_one_liner_list(obj):
+            return json.dumps(obj)
+        next_indent = current_indent + indent
+        output_list = []
+        output_list.append("[\n")
+        for i, value in enumerate(obj):
+            output_list.append(next_indent * " " + format(value, indent, next_indent))
+            if i != len(obj) - 1:
+                output_list.append(",\n")
+        output_list.append("\n" + current_indent * " " + "]")
+        return ''.join(output_list)
+    if isinstance(obj, dict):
+        if one_liner_dict_allowed and is_one_liner_dict(obj):
+            return json.dumps(obj)
+        if one_liner_dict_allowed and len(obj) == 1:
+            key, value = next(iter(obj.items()))
+            return '{' + json.dumps(key) + ': ' + format(value, indent, current_indent, one_liner_dict_allowed=False) + '}'
+        next_indent = current_indent + indent
+        output_list = []
+        output_list.append("{\n")
+        keys = list(obj.keys())
+        for i, key in enumerate(keys):
+            value = obj[key]
+            output_list.append(next_indent * ' ' + json.dumps(key) + ": " + format(value, indent, next_indent, one_liner_dict_allowed=False))
+            if i != len(keys) - 1:
+                output_list.append(",\n")
+        output_list.append('\n' + current_indent * " " + "}")
+        return ''.join(output_list)
+    raise ValueError("Unexpected object type {}: {}".format(type(obj), obj))
+
+# data = {
+#     "requires": [
+#         {"heatFrames": 100},
+#         {"or": [
+#             "canTrickyJump",
+#             {"and": [
+#                 "Morph",
+#                 "canWalljump",
+#                 {"shinespark": {
+#                     "frames": 50,
+#                 }}
+#             ]},
+#         ]}
+#     ],
+#     "clearsObstacles": ["A"],
+#     "exitCondition": {
+#         "leaveWithSpark": {}
+#     }
+# }
+# print(format(data, indent=2))
---
+++ scripts/split_region_files.py
@@ -0,0 +1,42 @@
+import json
+from pathlib import Path
+import os
+import copy
+
+import format_json
+
+def restructure_room(room_json):
+    new_room_json = copy.deepcopy(room_json)
+    strat_list = []
+    for link_json in new_room_json["links"]:
+        from_id = link_json["from"]
+        for to_json in link_json["to"]:
+            to_id = to_json["id"]
+            for strat in to_json["strats"]:
+                new_strat = {
+                    "from": from_id,
+                    "to": to_id,
+                    **strat,
+                }
+                strat_list.append(new_strat)
+    new_room_json["strats"] = strat_list
+    del new_room_json["links"]
+    return new_room_json
+
+for path in sorted(Path("../region/").glob("**/*.json")):
+    region_json = json.load(path.open("r"))
+    if region_json.get("$schema") != "../../schema/m3-region.schema.json":
+        continue
+
+    print("Processing", path)
+    base_name = path.name.removesuffix('.json')
+
+    # Create a new directory to hold room files
+    region_dir = path.parent / base_name
+    region_dir.mkdir(exist_ok=True)
+
+    for room_json in region_json["rooms"]:
+        room_name = room_json["name"].replace('/', 'or')
+        room_path = region_dir / (room_name + ".json")
+        new_room_json = restructure_room(room_json)
+        room_path.write_text(format_json.format(new_room_json, indent=2))
