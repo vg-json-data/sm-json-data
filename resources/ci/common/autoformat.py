d7cdd86dacff4ebd98bf8cd9bb1579b31d35f6b4
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2023-10-23 20:10:37 -0700

    Split rooms

---
+++ resources/ci/common/autoformat.py
@@ -0,0 +1,26 @@
+# Tool to auto-format all the region files in a standard way.
+#
+# To use, run "python autoformat.py" from a working directory of "sm-json-data/scripts".
+
+import json
+from pathlib import Path
+
+import format_json
+
+rootPath = "."
+regionPath = f"{rootPath}/region/"
+regionJSONs = sorted(Path(regionPath).glob("**/*.json"))
+
+for path in regionJSONs:
+    region_json = json.load(path.open("r"))
+    if region_json.get("$schema") != "../../schema/m3-region.schema.json":
+        continue
+
+    print("Processing", path)
+    new_region_json = format_json.format(region_json, indent=2)
+
+    # Validate that the new JSON is equivalent to the old (i.e. the differences affect formatting only):
+    assert json.loads(new_region_json) == region_json
+
+    # Write the auto-formatted output:
+    path.write_text(new_region_json)
