5f9ecf0666537e7922be679020d4780a07984e4c
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2023-10-29 02:03:44 -0700

    Fix working/running dir

--- scripts/autoformat.py
+++ scripts/autoformat.py
@@ -7,19 +7,22 @@ from pathlib import Path
 
 import format_json
 
-for dirpath in ["./region/", "./resources/app/manifests/"]:
+for dirpath in ["../region/", "../resources/app/manifests/"]:
     for filext in [".json", ".off"]:
         fileList = Path(dirpath).glob(f"**/*{filext}")
         for path in sorted(Path(dirpath).glob(f"**/*{filext}")):
-            region_json = json.load(path.open("r"))
-            # if "$schema" in region_json and region_json.get("$schema") != "../../schema/m3-region.schema.json":
-            #     continue
+            json_data = []
+            json_data = json.load(path.open("r"))
+            if "$schema" in json_data and json_data.get("$schema") not in [
+                "../../schema/m3-region.schema.json"
+            ]:
+                continue
 
             print("Processing", path)
-            new_region_json = format_json.format(region_json, indent=2)
+            new_json_data = format_json.format(json_data, indent=2)
 
             # Validate that the new JSON is equivalent to the old (i.e. the differences affect formatting only):
-            assert json.loads(new_region_json) == region_json
+            assert json.loads(new_json_data) == json_data
 
             # Write the auto-formatted output:
-            path.write_text(new_region_json)
+            path.write_text(new_json_data)
