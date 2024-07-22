f6e1f0678a44a8cc12049e102400caba660f5653
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2024-07-22 13:24:00 -0700

    Update Lookups

--- scripts/autoformat.py
+++ scripts/autoformat.py
@@ -7,16 +7,21 @@ from pathlib import Path
 
 import format_json
 
-for path in sorted(Path("../region/").glob("**/*.json")):
-    room_json = json.load(path.open("r"))
-    if room_json.get("$schema") != "../../../schema/m3-room.schema.json":
-        continue
+processPaths = [
+    "../region/",
+    "../resources/app/manifests/"
+]
+for processPath in processPaths:
+    for path in sorted(Path(processPath).glob("**/*.json")):
+        room_json = json.load(path.open("r"))
+        # if room_json.get("$schema") != "../../../schema/m3-room.schema.json":
+        #     continue
 
-    print("Processing", path)
-    new_room_json = format_json.format(room_json, indent=2)
+        print("Processing", path)
+        new_room_json = format_json.format(room_json, indent=2)
 
-    # Validate that the new JSON is equivalent to the old (i.e. the differences affect formatting only):
-    assert json.loads(new_room_json) == room_json
+        # Validate that the new JSON is equivalent to the old (i.e. the differences affect formatting only):
+        assert json.loads(new_room_json) == room_json
 
-    # Write the auto-formatted output:
-    path.write_text(new_room_json)
+        # Write the auto-formatted output:
+        path.write_text(new_room_json)
