8fe982c321e63cb69f90ef3a6dd42e18c3a0a22f
Brent Kerby <blkerby@gmail.com>
2024-08-06 08:32:26 -0600

    Print output for changed files only

--- scripts/populate_strat_ids.py
+++ scripts/populate_strat_ids.py
@@ -9,13 +9,13 @@ from pathlib import Path
 import format_json
 
 for path in sorted(Path("../region/").glob("**/*.json")):
-    room_json = json.load(path.open("r"))
+    room_str = path.open("r").read()
+    room_json = json.loads(room_str)
     if room_json.get("$schema") != "../../../schema/m3-room.schema.json":
         continue
 
-    print("Processing", path)
-    
     next_id = room_json.get("nextStratId", 1)
+    orig_next_id = next_id
     for i in range(len(room_json["strats"])):
         strat = room_json["strats"][i]
         if "id" not in strat:
@@ -24,7 +24,13 @@ for path in sorted(Path("../region/").glob("**/*.json")):
             next_id += 1
     room_json["nextStratId"] = next_id
     
-    new_room_json = format_json.format(room_json, indent=2)
+    new_room_str = format_json.format(room_json, indent=2)
+
+    if next_id != orig_next_id:
+        print("Added {} strat IDs in {}".format(next_id - orig_next_id, path))
+    elif room_str != new_room_str:
+        print("Formatted {}".format(path))
     
     # Write the auto-formatted output:
-    path.write_text(new_room_json)
+    if room_str != new_room_str:
+        path.write_text(new_room_str)
