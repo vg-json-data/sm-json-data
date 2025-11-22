145d6bc10b9b91759bc388db129ae58cd551280c
aquanight <aquanight@gmail.com>
2025-11-22 10:09:47 -0700

    Introduce helper for heat-trigger spark interrupts.

--- tests/asserts/keywords.py
+++ tests/asserts/keywords.py
@@ -504,7 +504,7 @@ def process_req_speed_state(req, states, err_fn):
             states = {"preshinespark"}
         elif req in ["h_CrystalSpark", "h_CrystalSparkWithoutLenience",
                      "h_underwaterCrystalSpark", "h_underwaterCrystalSparkWithoutLenience", "h_heatedCrystalSpark",
-                     "canRModeSparkInterrupt", "canRModePauseAbuseSparkInterrupt", "h_RModeKnockbackSpark"]:
+                     "canRModeSparkInterrupt", "canRModePauseAbuseSparkInterrupt", "h_RModeKnockbackSpark", "h_heatTriggerRModeSparkInterrupt"]:
             if not states.issubset(["shinecharging", "shinecharged", "preshinespark"]):
                 err_fn(f"{req} while not shinecharging/shinecharged/preshinespark")
             states = {"normal"}
