6cd66aa28ec76627af982d3486a5b1e4e4196ef4
kjbranch <kylejbranch@gmail.com>
2025-12-30 12:01:31 -0700

    remove links schema and in tests

--- tests/asserts/keywords.py
+++ tests/asserts/keywords.py
@@ -252,25 +252,6 @@ def process_keyvalue(k, v, metadata):
                     messages["counts"]["reds"] += 1
     return goodValue
 
-# try to navigate a path between nodes if no direct path
-def search_for_path(fromNodes, sourceNode, targetNode, stratRef):
-    foundPath = False
-    msg = ""
-    for tNode in fromNodes[str(sourceNode)]["to"]:
-        if not foundPath:
-            # print(f"Testing {sourceNode}:{tNode}:{targetNode}")
-            if (str(tNode) in roomData["links"]["from"]) and \
-                (str(targetNode) in roomData["links"]["from"][str(tNode)]["to"]):
-                foundPath = True
-                # msg = f"🟢Found Path:{stratRef}::{sourceNode}:{tNode}:{targetNode}"
-                # print(msg)
-                # messages["greens"].append(msg)
-                # messages["counts"]["greens"] += 1
-    if not foundPath:
-        msg = f"🟡WARNING: Path not found:{stratRef}::{sourceNode}:{tNode}:{targetNode}::Is it longer than a 3-node chain? Gave up looking"
-
-    return [foundPath, msg]
-
 
 def find_door_unlocked_nodes_rec(req):
     if isinstance(req, dict):
@@ -733,9 +714,6 @@ for r,d,f in os.walk(os.path.join(".","region")):
                         "subarea": subarea,
                         "subsubarea": subsubarea,
                         "fullarea": fullarea,
-                        "links": {
-                            "from": {}
-                        },
                         "nodes": {
                             "froms": [],
                             "tos": [],
@@ -899,22 +877,6 @@ for r,d,f in os.walk(os.path.join(".","region")):
                                 messages["reds"].append(msg)
                                 messages["counts"]["reds"] += 1
 
-                    # Document Links
-                    link_set = set()
-                    for link_from in room["links"]:
-                        from_node_id = link_from["from"]
-                        if from_node_id not in roomData["nodes"]["ids"]:                            
-                            msg = f"🔴ERROR: In links, from node {from_node_id} doesn't exist."
-                            messages["reds"].append(msg)
-                            messages["counts"]["reds"] += 1
-                        for link in link_from["to"]:
-                            to_node_id = link["id"]
-                            if to_node_id not in roomData["nodes"]["ids"]:                            
-                                msg = f"🔴ERROR: In links, to node {to_node_id} doesn't exist."
-                                messages["reds"].append(msg)
-                                messages["counts"]["reds"] += 1
-                            link_set.add((from_node_id, to_node_id))
-
                     # Document Link Strats
                     for strat in room["strats"]:
                         if "link" not in strat:
@@ -923,11 +885,6 @@ for r,d,f in os.walk(os.path.join(".","region")):
                             messages["counts"]["reds"] += 1
                             continue
                         link = strat["link"]
-                        linkRef = str(link)
-                        if tuple(strat["link"]) not in link_set:
-                            msg = f"🔴ERROR: Link {linkRef} doesn't exist: {roomRef}:{strat['name']}"
-                            messages["reds"].append(msg)
-                            messages["counts"]["reds"] += 1
                         toNode = link[1]
                         roomData["nodes"]["tos"].append(toNode)
 
@@ -1055,7 +1012,7 @@ for r,d,f in os.walk(os.path.join(".","region")):
                     used_notable_name_set = set()
                     link_strat_names = set()
                     for strat in room["strats"]:
-                        if "link" not in strat or tuple(strat["link"]) not in link_set:
+                        if "link" not in strat:
                             # Errors are already generated above in this case.
                             continue
                         link = strat["link"]
